0635ae85f48c837913294370295423af5073c8d8
sostenes198br <sostenes198br@hotmail.com>
2019-05-23 23:26:09 -0300

    Projeto Selenium

--- Estudos.Selenium/SeleniumTest.cs
+++ Estudos.Selenium/SeleniumTest.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
@@ -7,30 +8,32 @@ using Xunit;
 
 namespace Estudos.Selenium
 {
-   public class SeleniumTest : IDisposable
+    public class SeleniumTest
     {
-        private readonly ChromeDriver _driver;
         public SeleniumTest()
         {
-            _driver = new ChromeDriver(Environment.CurrentDirectory);
+
         }
 
         [Fact]
         public void Selenium_Teste()
         {
+            ChromeDriver _driver = new ChromeDriver(Environment.CurrentDirectory);
             try
             {
-                _driver.Navigate().GoToUrl("http://sqhoras:8085");
+                //_driver.Navigate().GoToUrl("http://sqhoras:8085");
+                _driver.Navigate().GoToUrl("http://www.squadra.com.br/sqhoras/");
                 _driver.FindElementById("spnusuario").SendKeys(Constantes.Login);
                 _driver.FindElementById("spnsenha").SendKeys(Constantes.Senha);
                 _driver.FindElementById("acaoBotao").Click();
 
-                _driver.Navigate().GoToUrl("http://sqhoras:8085/timesheetColaborador.asp?date=5/22/2019");
+                //_driver.Navigate().GoToUrl("http://sqhoras:8085/timesheetColaborador.asp?date=5/22/2019");
+                _driver.Navigate().GoToUrl("http://www.squadra.com.br/sqhoras/timesheetColaborador.asp?date=5/22/2019");
                 _driver.FindElementByXPath("//td[@nomealocacao='28563-SOFTPLAN - SQUAD 1 - NÚCLEO BH .NET/ATUAÇÃO TIME 2019/ANALISTA DESENVOLVEDOR']").Click();
 
                 var elementos = _driver.FindElementsByXPath("//td[@height='1']").Select(lnq => TimeSpan.Parse(lnq.Text)).ToList();
 
-                if (elementos.Count % 2 != 0)
+                if(elementos.Count % 2 != 0)
                 {
                     _driver.ExecuteScript("alert('Sua régua está bugada amigão !!!')");
                     Thread.Sleep(3000);
@@ -38,43 +41,103 @@ namespace Estudos.Selenium
                 }
 
                 var periodos = ListarPeriodos(elementos);
-                var maiorPeriodo = ObterMaiorPeriodoDeTempo(periodos);
+                ObterMaiorIntervalo(periodos);
+                //var maiorPeriodo = ObterMaiorPeriodoDeTempo(periodos);
 
 
 
             }
             catch
             {
+            }
+            finally
+            {
                 _driver.Quit();
             }
 
         }
 
-        public void Dispose()
+        [Theory]
+        [MemberData(nameof(Cenarios_Contruir_Periodo_Almoco))]
+        public void Deve_Construir_Periodo_Almoco_Valido(List<Periodo> periodos, List<Periodo> periodosValido)
         {
-            _driver.Quit();
+            var indice = 0;
+
+            ConstruiPeriodoAlmoco(periodos, indice);
+
+            periodos.Should().BeEquivalentTo(periodosValido);
+        }
+
+        public void Deve_Definir_Periodo_Almoco_Valido()
+        {
+
         }
 
         private List<Periodo> ListarPeriodos(List<TimeSpan> elementos)
         {
             var periodos = new List<Periodo>();
-            for (var i = 0; i < elementos.Count; i += 2)
+            for(var i = 0; i < elementos.Count; i += 2)
                 periodos.Add(new Periodo(elementos[i], elementos[i + 1]));
 
             return periodos;
         }
 
-        private (int indice, TimeSpan periodo) ObterMaiorPeriodoDeTempo(List<Periodo> periodos)
+        private void ObterMaiorIntervalo(List<Periodo> periodos)
         {
-            (int indice, TimeSpan periodo) periodo = (0, periodos[0].PeriodoFinal.Subtract(periodos[0].PeriodoInicial));
-            for(var i = 1; i < periodos.Count; i++)
-            {
-                var periodoAtual = periodos[i].PeriodoFinal.Subtract(periodos[i].PeriodoInicial);
-                if (periodoAtual.CompareTo(periodo.periodo) == 1)
-                    periodo = (i, periodoAtual);
-            }            
+            var meioDia = new TimeSpan(12, 0, 0);
 
-            return periodo;
+            var valorIniciallMaisProximoMeioDia = periodos.Select(lnq => lnq.PeriodoInicial).First(lnq => lnq.CompareTo(meioDia) >= 0);
+            var valorFinallMaisProximoMeioDia = periodos.Select(lnq => lnq.PeriodoFinal).First(lnq => meioDia.CompareTo(lnq) <= 0);
+
+            var indiceParteManha = periodos.IndexOf(periodos.First(lnq => lnq.PeriodoFinal.CompareTo(valorFinallMaisProximoMeioDia) == 0));
+            var indiceParteTarde = periodos.IndexOf(periodos.First(lnq => lnq.PeriodoInicial.CompareTo(valorIniciallMaisProximoMeioDia) == 0));
+
+            if(indiceParteManha == indiceParteTarde)
+                ConstruiPeriodoAlmoco(periodos, indiceParteManha);
+
+            else
+                DefinirPeriodoAlmoco(periodos, indiceParteManha, indiceParteTarde);
+
         }
+
+        private void ConstruiPeriodoAlmoco(List<Periodo> periodoAlmoco, int indice)
+        {
+            var meioDia = new TimeSpan(12, 0, 0);
+            if(periodoAlmoco[indice].PeriodoFinal.Subtract(periodoAlmoco[indice].PeriodoInicial).CompareTo(new TimeSpan(6, 0, 0)) <= 0)
+                return;
+
+            if(periodoAlmoco[indice].PeriodoInicial.CompareTo(meioDia) == -1 && (meioDia.Subtract(periodoAlmoco[indice].PeriodoInicial).CompareTo(new TimeSpan(2, 0, 0)) >= 0))
+            {
+                var periodoParteTarde = new Periodo(new TimeSpan(13, 0, 0), periodoAlmoco[indice].PeriodoFinal);
+                periodoAlmoco[indice].PeriodoFinal = new TimeSpan(12, 0, 0);
+                periodoAlmoco.Insert(indice + 1, periodoParteTarde);
+            }
+            else
+            {
+                var periodoParteTarde = new Periodo(periodoAlmoco[indice].PeriodoInicial.Add(new TimeSpan(3, 0, 0)), periodoAlmoco[indice].PeriodoFinal);
+                periodoAlmoco[indice].PeriodoFinal = periodoParteTarde.PeriodoInicial.Subtract(new TimeSpan(1, 0, 0));
+                periodoAlmoco.Insert(indice + 1, periodoParteTarde);
+            }
+        }
+
+        private void DefinirPeriodoAlmoco(List<Periodo> periodos, int indiceParteManha, int indiceParteTarde)
+        {
+            var umaHora = new TimeSpan(1, 0, 0);
+
+            if(periodos[indiceParteTarde].PeriodoInicial.Subtract(periodos[indiceParteManha].PeriodoFinal).CompareTo(umaHora) == -1)
+                periodos[indiceParteTarde].PeriodoInicial = periodos[indiceParteManha].PeriodoFinal.Add(umaHora);
+
+        }
+
+        public static IEnumerable<object[]> Cenarios_Contruir_Periodo_Almoco =>
+            new List<object[]>
+            {
+                 new object[]{new List<Periodo> { new Periodo(new TimeSpan(8, 0, 0), new TimeSpan(17, 0, 0)) }, new List<Periodo> { new Periodo(new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0)), new Periodo(new TimeSpan(13, 0, 0), new TimeSpan(17, 0, 0)) } },
+                 new object[]{new List<Periodo> { new Periodo(new TimeSpan(8, 0, 0), new TimeSpan(15, 0, 0)) }, new List<Periodo> { new Periodo(new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0)), new Periodo(new TimeSpan(13, 0, 0), new TimeSpan(15, 0, 0)) } },
+                 new object[]{new List<Periodo> { new Periodo(new TimeSpan(11, 0, 0), new TimeSpan(20, 0, 0)) }, new List<Periodo> { new Periodo(new TimeSpan(11, 0, 0), new TimeSpan(13, 0, 0)), new Periodo(new TimeSpan(14, 0, 0), new TimeSpan(20, 0, 0)) } },
+                 new object[]{new List<Periodo> { new Periodo(new TimeSpan(12, 0, 0), new TimeSpan(20, 0, 0)) }, new List<Periodo> { new Periodo(new TimeSpan(12, 0, 0), new TimeSpan(14, 0, 0)), new Periodo(new TimeSpan(15, 0, 0), new TimeSpan(20, 0, 0)) } },
+                 new object[]{new List<Periodo> { new Periodo(new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0)) }, new List<Periodo> { new Periodo(new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0)) } },
+                 new object[]{new List<Periodo> { new Periodo(new TimeSpan(12, 0, 0), new TimeSpan(17, 0, 0)) }, new List<Periodo> { new Periodo(new TimeSpan(12, 0, 0), new TimeSpan(17, 0, 0)) } },                 
+            };
     }
 }
