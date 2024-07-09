887db6d01fde411e2130da02d15273016612d176
Sóstenes Souza <sostenes.souza@govoll.com>
2024-07-09 15:24:04 -0300

    Estudos

--- Projetos-Schedule-Message/src/Scheduled.Message.Api/Program.cs
+++ Projetos-Schedule-Message/src/Scheduled.Message.Api/Program.cs
@@ -1,47 +1,103 @@
 using System.Diagnostics.CodeAnalysis;
 using Hangfire;
+using NewRelic.LogEnrichers.Serilog;
 using Scheduled.Message.Api.Bootstrappers;
 using Scheduled.Message.Api.Hangfire.Dashboard.Authorization;
 using Scheduled.Message.Api.HealthCheck;
+using Scheduled.Message.Api.Serilog;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddControllers();
-
-builder.Services
-    .AddEndpointsApiExplorer()
-    .AddSwaggerGen();
-
-builder.Services.AddHttpContextAccessor();
-
-builder.Services.AddAppHealthChecks(builder.Configuration);
-
-builder.Services.BootstrapperApplication(builder.Configuration);
-
-var app = builder.Build();
-
-app.UseRouting();
-app.MapControllers();
-app.MapHealthChecks();
-app.MapHangfireDashboard(new DashboardOptions
+try
 {
-    Authorization = new[] { new DashboardNoAuthorizationFilter() },
-});
+    Log.Information("Starting host");
 
-app.UseSwagger();
-app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
 
-app.UseHttpsRedirection();
+    builder.Services.AddControllers();
 
-using (var scope = app.Services.CreateScope())
+    builder.Services
+        .AddEndpointsApiExplorer()
+        .AddSwaggerGen();
+
+    builder.Services.AddHttpContextAccessor();
+
+    builder.Services.AddAppHealthChecks(builder.Configuration);
+
+    builder.Services.BootstrapperApplication(builder.Configuration);
+    
+    builder.Services.AddSerilog((sp, loggerConfiguration) =>
+    {
+        var configuration = sp.GetRequiredService<IConfiguration>();
+
+        loggerConfiguration
+            .MinimumLevel.Override("Default", Enum.Parse<LogEventLevel>(configuration["LOG_LEVEL_DEFAULT"]!))
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore.HealthChecks.UI", LogEventLevel.Verbose)
+            .MinimumLevel.Override("HealthChecks", LogEventLevel.Verbose)
+            .MinimumLevel.Override("Hangfire", LogEventLevel.Information);
+
+        loggerConfiguration
+            .Enrich.WithNewRelicLogsInContext()
+            .Enrich.WithExceptionDetails()
+            .Enrich.WithMachineName()
+            .Enrich.WithProcessId()
+            .Enrich.WithProcessName()
+            .Enrich.WithCorrelationId()
+            .Enrich.WithCorrelationIdHeader()
+            .Enrich.WithThreadId()
+            .Enrich.WithThreadName();
+
+        loggerConfiguration.WriteTo.Console(formatter: new NewRelicFormatter());
+    });
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging(opts =>
+    {
+        opts.EnrichDiagnosticContext = SerilogHelper.EnrichFromRequest;
+        opts.GetLevel = SerilogHelper.CustomGetLevel;
+    });
+
+    app.UseRouting();
+    app.MapControllers();
+    app.MapHealthChecks();
+    app.MapHangfireDashboard(new DashboardOptions
+    {
+        Authorization = new[] { new DashboardNoAuthorizationFilter() },
+    });
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.UseHttpsRedirection();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        MongoDbInitialize.InitializeAsync(scope.ServiceProvider, CancellationToken.None).Wait();
+    }
+
+    app.Run();
+}
+catch (Exception ex)
 {
-    MongoDbInitialize.InitializeAsync(scope.ServiceProvider, CancellationToken.None).Wait();
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.Run();
 
-
-namespace Scheduled.Message.Api
+namespace Hangfire.Server.Api
 {
     [ExcludeFromCodeCoverage]
     public partial class Program;
---
+++ Projetos-Schedule-Message/src/Scheduled.Message.Api/Serilog/SerilogHelper.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Serilog;
+using Serilog.Events;
+
+namespace Scheduled.Message.Api.Serilog;
+
+[ExcludeFromCodeCoverage]
+public static class SerilogHelper
+{
+    public static void EnrichFromRequest(IDiagnosticContext diagnosticContext, HttpContext httpContext)
+    {
+        var request = httpContext.Request;
+
+        diagnosticContext.Set("Host", request.Host);
+        diagnosticContext.Set("Protocol", request.Protocol);
+        diagnosticContext.Set("Scheme", request.Scheme);
+
+        if (request.QueryString.HasValue)
+        {
+            diagnosticContext.Set("QueryString", request.QueryString.Value);
+        }
+
+        diagnosticContext.Set("ContentType", httpContext.Response.ContentType);
+
+        var headers = request.Headers;
+        if (headers.Any())
+            diagnosticContext.Set("Headers", headers);
+    }
+
+    public static LogEventLevel CustomGetLevel(HttpContext ctx, double _, Exception? ex) =>
+        ex != null
+            ? LogEventLevel.Error
+            : ctx.Response.StatusCode > 499
+                ? LogEventLevel.Error
+                : IsEndpointToIgnore(ctx) // Not an error, check if it was a health check
+                    ? LogEventLevel.Verbose // Was a health check, use Verbose
+                    : LogEventLevel.Information;
+
+    private static bool IsEndpointToIgnore(HttpContext ctx)
+    {
+        var path = ctx.Request.Path.Value;
+        if (string.IsNullOrEmpty(path)) return false;
+
+        return path.Contains("swagger", StringComparison.OrdinalIgnoreCase)
+               || path.Contains("health", StringComparison.OrdinalIgnoreCase)
+               || path.Contains("hangfire", StringComparison.OrdinalIgnoreCase);
+    }
+}
